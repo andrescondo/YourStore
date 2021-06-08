095c51548c802ec7313d921e7532e6624708e791
andrescondo <1dkevinandres.b@gmail.com>
2021-06-08 18:14:31 -0500

    añadidos estilos al landing

--- src/containers/Landing.jsx
+++ src/containers/Landing.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 import '../styles/components/landing.css';
 
 const Landing = () => {
-  return <div className="Landing"></div>;
+  return (
+    <div className="Landing">
+      <div className="hero">
+        <h2>
+          <strong>Your Store</strong>
+          <br />
+        </h2>
+        <p>
+          Almacena tus productos de manera completamente online. <br />
+          Registarte, y accede a tener tu propio inventariado.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
