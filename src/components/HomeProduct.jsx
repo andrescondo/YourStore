9bba3e9a64f8269cde9096d36db9ebe9b537859d
andrescondo <1dkevinandres.b@gmail.com>
2021-06-10 18:13:03 -0500

    creacion de estructura de componente HomeProduct

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import '../styles/components/HomeProduct.css';
+
+function Press() {
+  alert('Hola');
+}
+
 const HomeProduct = () => {
-  return <h3>Hola como estas - Porductos</h3>;
+  return (
+    <div className="HomeProduct">
+      <div className="create">
+        <input type="button" value="Crear Producto" onClick={Press} />
+      </div>
+      <div className="boxCreate">
+        <div className="boxProdcut">
+          <p>Nombre</p>
+          <p>Código</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeProduct;
