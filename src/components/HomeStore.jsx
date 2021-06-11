e010707ee74a52ec6973ee23a54137702aea467f
andrescondo <1dkevinandres.b@gmail.com>
2021-06-11 15:17:49 -0500

    creado card de boxStore, aplicado estilos y ubicacion de botones

--- src/components/HomeStore.jsx
+++ src/components/HomeStore.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import '../styles/components/HomeStore.css';
+
 function Press() {
   alert('Hola');
 }
 
 const HomeStore = () => {
   return (
-    <div className="boxStore">
+    <div className="HomeStore">
       <div className="create">
         <input type="button" value="Crear Bodega" onClick={Press} />
       </div>
       <div className="createStore">
         <div className="boxStore">
+          <div className="boxStore-icon">
+            <i className="fas fa-plus"></i>
+          </div>
           <div className="boxStore-info">
             <p>
               Nombre de bodega: <span>DFRTS</span>
@@ -19,6 +24,10 @@ const HomeStore = () => {
             <p>
               ID: <span>SF3FSD3</span>
             </p>
+          </div>
+          <div className="boxProduct-icons">
+            <i className="fas fa-edit"></i>
+            <i className="fas fa-trash"></i>
           </div>
         </div>
       </div>
