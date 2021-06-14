fac77ffb123114b46254ce331e021e16aa377d19
andrescondo <1dkevinandres.b@gmail.com>
2021-06-14 00:38:35 -0500

    añadida funcionalidad para cerrar haciendo click en fondo semitransparente

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Modal = ({ children, isOpen, closeModal }) => {
-  // const handleModalContainerClick = (e) => e.stopPropagation();
+  const handleModalContainerClick = (e) => e.stopPropagation();
 
   return (
-    <article className={`modal ${isOpen && 'is-open'}`}>
-      <div className="modal-container">
+    <article className={`modal ${isOpen && 'is-open'}`} onClick={closeModal}>
+      <div className="modal-container" onClick={handleModalContainerClick}>
         <button className="modal-close" onClick={closeModal}>
           X
         </button>
