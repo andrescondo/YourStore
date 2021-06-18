acf2198f2635d28b3968d6523bb30c3442ec74a8
andrescondo <1dkevinandres.b@gmail.com>
2021-06-17 22:19:33 -0500

    aplicando valores y funciones inciales del form

--- src/components/HomeFrom.jsx
+++ src/components/HomeFrom.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/components/HomeFrom.css';
-import '../styles/components/Modals.css';
 
 import ModalButton from './ModalButton';
 
+const initialForm = {
+  name: '',
+  code: '',
+  id: null,
+};
+
 const HomeFrom = ({ title, name, code }) => {
+  const [form, setForm] = useState(initialForm);
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
   return (
     <div className="HomeProduct-form">
       <h3>{title}</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">
           <p>{name}</p>
-          <input type="text" placeholder={name} />
+          <input
+            type="text"
+            placeholder={name}
+            onChange={handleChange}
+            value={form.name}
+          />
         </label>
         <label htmlFor="code">
           <p>{code}</p>
-          <input type="text" placeholder={code} />
+          <input
+            type="text"
+            placeholder={code}
+            onChange={handleChange}
+            value={form.code}
+          />
         </label>
         <ModalButton />
       </form>
