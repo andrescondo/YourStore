03fc249fa883b4bdea4d5c6b858917f4b5111556
andrescondo <1dkevinandres.b@gmail.com>
2021-06-30 14:01:03 -0500

    CREACION DE CUSTOM HOOK para el crudData del boxProduct

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { helpHttp } from '../helpers/helpHttp';
+import React from 'react';
+// import { helpHttp } from '../helpers/helpHttp';
+import useCrudData from '../hooks/useCrudData';
 
 import '../styles/components/HomeProduct.css';
 
@@ -9,106 +10,20 @@ import LoadingData from './LoadingData';
 import ErrorData from './ErrorData';
 
 const HomeProduct = () => {
-  const [db, setDb] = useState([]);
-  const [formState, setFormState] = useState(false);
-  const [dataToEdit, setDataToEdit] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [updateDb, setUpdateDb] = useState([]);
-  // const [updateDb, setUpdateDb] = useState([]);
-
-  const api = helpHttp();
   const url = 'http://localhost:3004/products';
-  //asegurarse  ^^ que el protocolo sea el correcto
 
-  useEffect(() => {
-    helpHttp()
-      .get(url)
-      .then((res) => {
-        setLoading(true);
-        if (!res.err) {
-          setDb(res);
-          setError(null);
-          // setUpdateDb(!updateDb);
-        } else {
-          setDb(null);
-          setError(res);
-        }
-        setLoading(false);
-        console.log(res.length);
-        // console.log(res.length + 1);
-      });
-    // setUpdateDb(!updateDb);
-  }, [updateDb]);
-  //  ^^^ Importante, se usa este hooks de estado para actualizar el effect,
-  //cambia de valor siempre que se actualice o cree un nuevo dato,
-  //permitiendo asi renderizar los componentes nuevos
-
-  const createData = (data) => {
-    data.id = `${db.length + 1}-${Date.now()}`;
-    // data.id = db.length + 1+ Date.now();
-    // console.log(db.length + 1);
-    let options = {
-      body: data,
-      headers: { 'content-type': 'application/json' },
-    };
-    api.post(url, options).then((res) => {
-      console.log(res);
-      if (!res.err) {
-        setDb(...db, res);
-        setUpdateDb(res);
-        // renderiza los elementos una vez creados
-        //NOTA: optimizar en el networks mas adelante
-      } else {
-        console.log(res.err);
-      }
-    });
-    // setUpdateDb(false);
-    // setDb([...db, data]);
-  };
-
-  const updateData = (data) => {
-    let endpoint = `${url}/${data.id}`;
-    let options = {
-      body: data,
-      headers: { 'content-type': 'application/json' },
-    };
-
-    api.put(endpoint, options).then((res) => {
-      console.log(res);
-      if (!res.err) {
-        let newData = db.map((d) => (d.id === data.id ? data : d));
-        setDb(newData);
-        // setDb(...db, res);
-        setUpdateDb(newData);
-        // renderiza los elementos una vez creados
-        //NOTA: optimizar en el networks mas adelante
-      } else {
-        console.log(res.err);
-      }
-    });
-  };
-
-  const deleteData = (id) => {
-    let endpoint = `${url}/${id}`;
-    let options = {
-      headers: { 'content-type': 'application/json' },
-    };
-    api.del(endpoint, options).then((res) => {
-      if (!res.err) {
-        let newData = db.filter((el) => el.id !== id);
-        setDb(newData);
-      } else {
-        setError(res);
-      }
-    });
-    //se usa el click del modal para validar si quiere eliminar o no
-  };
-
-  const openForm = () => {
-    setFormState(!formState);
-    // console.log(formState);
-  };
+  const {
+    db,
+    deleteData,
+    loading,
+    formState,
+    openForm,
+    updateData,
+    createData,
+    dataToEdit,
+    setDataToEdit,
+    error,
+  } = useCrudData(url);
 
   return (
     <div className="HomeProduct">
---
+++ src/hooks/useCrudData.js
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
+
+export default function useCrudData(url) {
+  // const url = url;
+  const [db, setDb] = useState([]);
+  const [formState, setFormState] = useState(false);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [updateDb, setUpdateDb] = useState([]);
+
+  const api = helpHttp();
+  // const url = 'http://localhost:3004/products';MANDAR ESTE DATO DESDE EL COMPONENTE
+  //asegurarse  ^^ que el protocolo sea el correcto
+
+  useEffect(() => {
+    helpHttp()
+      .get(url)
+      .then((res) => {
+        setLoading(true);
+        if (!res.err) {
+          setDb(res);
+          setError(null);
+        } else {
+          setDb(null);
+          setError(res);
+        }
+        setLoading(false);
+      });
+  }, [updateDb]);
+  //  ^^^ Importante, se usa este hooks de estado para actualizar el effect,
+  //cambia de valor siempre que se actualice o cree un nuevo dato,
+  //permitiendo asi renderizar los componentes nuevos
+
+  const createData = (data) => {
+    data.id = `${db.length + 1}-${Date.now()}`;
+    let options = {
+      body: data,
+      headers: { 'content-type': 'application/json' },
+    };
+    api.post(url, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        setDb(...db, res);
+        setUpdateDb(res);
+        // renderiza los elementos una vez creados
+        //NOTA: optimizar en el networks mas adelante
+      } else {
+        console.log(res.err);
+      }
+    });
+  };
+
+  const updateData = (data) => {
+    let endpoint = `${url}/${data.id}`;
+    let options = {
+      body: data,
+      headers: { 'content-type': 'application/json' },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        let newData = db.map((d) => (d.id === data.id ? data : d));
+        setDb(newData);
+        // setDb(...db, res);
+        setUpdateDb(newData);
+        // renderiza los elementos una vez creados
+        //NOTA: optimizar en el networks mas adelante
+      } else {
+        console.log(res.err);
+      }
+    });
+  };
+
+  const deleteData = (id) => {
+    let endpoint = `${url}/${id}`;
+    let options = {
+      headers: { 'content-type': 'application/json' },
+    };
+    api.del(endpoint, options).then((res) => {
+      if (!res.err) {
+        let newData = db.filter((el) => el.id !== id);
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
+    //se usa el click del modal para validar si quiere eliminar o no
+  };
+
+  const openForm = () => {
+    setFormState(!formState);
+    // console.log(formState);
+  };
+
+  return {
+    db,
+    deleteData,
+    loading,
+    formState,
+    openForm,
+    updateData,
+    createData,
+    dataToEdit,
+    setDataToEdit,
+    error,
+  };
+}
